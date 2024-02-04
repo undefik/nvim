dbd0ced3016a47c9aa0200b218319f6ef2b62884
MichalNemecek <nemecekinfo@seznam.cz>
2024-02-04 18:14:55 +0100

    Added shiftwidth and re-indented all config files

--- init.lua
+++ init.lua
@@ -1,23 +1,23 @@
 require("lazybootstrap")
 require("lazy").setup({
-		{"RRethy/nvim-base16", lazy = false},
-		{"nvim-lualine/lualine.nvim", lazy = false},
-		{"lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}},
-		{"kaarmu/typst.vim", ft = "typst", lazy = false},
-		{"m4xshen/autoclose.nvim", lazy = false},
-		{"evanleck/vim-svelte", lazy = false},
-		{"nvim-neo-tree/neo-tree.nvim", branch = "v3.x", dependencies = {
-				"nvim-lua/plenary.nvim",
-				"nvim-tree/nvim-web-devicons",
-				"MunifTanjim/nui.nvim"
-		}},
-		{"ms-jpq/coq_nvim", lazy = false},
-		{"jidn/vim-dbml", lazy = false},
-		{"folke/which-key.nvim", lazy = false},
-		{"nvim-telescope/telescope.nvim", lazy = false},
-		{"akinsho/toggleterm.nvim", lazy = false},
-		{"tomiis4/Hypersonic.nvim", lazy = false},
-		{"L3MON4D3/LuaSnip", run = "make install_jsregexp"},
+	{"RRethy/nvim-base16", lazy = false},
+	{"nvim-lualine/lualine.nvim", lazy = false},
+	{"lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}},
+	{"kaarmu/typst.vim", ft = "typst", lazy = false},
+	{"m4xshen/autoclose.nvim", lazy = false},
+	{"evanleck/vim-svelte", lazy = false},
+	{"nvim-neo-tree/neo-tree.nvim", branch = "v3.x", dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim"
+	}},
+	{"ms-jpq/coq_nvim", lazy = false},
+	{"jidn/vim-dbml", lazy = false},
+	{"folke/which-key.nvim", lazy = false},
+	{"nvim-telescope/telescope.nvim", lazy = false},
+	{"akinsho/toggleterm.nvim", lazy = false},
+	{"tomiis4/Hypersonic.nvim", lazy = false},
+	{"L3MON4D3/LuaSnip", run = "make install_jsregexp"},
 })
 require("lualine-setup")
 require("autoclose").setup()
@@ -25,6 +25,7 @@ require("ibl").setup()
 require("toggleterm").setup()
 vim.cmd[[colorscheme base16-ayu-dark]]
 vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.cursorline = true
 vim.cmd[[:tnoremap <Esc> <C-\><C-n>]]
 require("whichkey")
--- lua/lualine-setup.lua
+++ lua/lualine-setup.lua
@@ -1,6 +1,6 @@
 require("lualine").setup({
-		 options = {
-				 component_separators = '',
-				 section_separators = ''
-		 }
+	options = {
+		component_separators = '',
+		section_separators = ''
+	}
 })
